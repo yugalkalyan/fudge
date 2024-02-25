3f91f84adebc2b5123bb48f58b946eb9ea481d78
Yugal Kalyan <yugalkalyan@gmail.com>
2024-02-25 12:51:26 +0530

    main branch

---
+++ android/app/src/main/kotlin/com/fudge/fudge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fudge.fudge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:fudge/first_screen/first_screen.dart';
+
+void main() {
+  runApp(const FirstScreen());
+}
+
